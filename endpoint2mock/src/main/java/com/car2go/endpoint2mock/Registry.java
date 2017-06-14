021977665cfc788f4cd535939d563beb97c4e26a
Dmitry Zaytsev <dmitry.zaytsev@daimler.com>
2017-06-14 17:37:02 +0200

    Implemented Registry.

--- endpoint2mock/src/main/java/com/car2go/endpoint2mock/Registry.java
+++ endpoint2mock/src/main/java/com/car2go/endpoint2mock/Registry.java
@@ -1,15 +1,53 @@
 package com.car2go.endpoint2mock;
 
+import java.lang.reflect.Method;
+
 /**
  * Checks whether URL should be mocked or not.
  */
 class Registry {
 
+    private final Class<?> generatedMocksRegistryClass;
+    private final Method isMockedMethod;
+
+    public Registry() {
+        generatedMocksRegistryClass = loadMocksRegistryClass();
+        isMockedMethod = loadIsMockedMethod(generatedMocksRegistryClass);
+    }
+
+    private static Method loadIsMockedMethod(Class<?> registryClass) {
+        if (registryClass == null) {
+            return null;
+        }
+
+        try {
+            return registryClass.getDeclaredMethod("isMocked", registryClass);
+        } catch (NoSuchMethodException e) {
+            return null;
+        }
+    }
+
+    private static Class<?> loadMocksRegistryClass() {
+        try {
+            return Class.forName("com.car2go.mock.MocksRegistry");
+        } catch (ClassNotFoundException e) {
+            return null;
+        }
+    }
+
     /**
      * @return {@code true} if given URL is in registry. {@code false} if it is not.
      */
     public boolean isInRegistry(String url) {
-        return true;
+        if (isMockedMethod == null) {
+            return false;
+        }
+
+        try {
+            return (boolean) isMockedMethod.invoke(generatedMocksRegistryClass, url);
+        } catch (Exception e) {
+            return false;
+        }
     }
 
 }
